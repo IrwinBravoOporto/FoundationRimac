d6a36427f25764978e819984c5da86741effbecd
Irwin <irwinbravooporto@gmail.com>
2023-04-14 14:45:28 -0500

    Initial Commit

---
+++ FoundationRimac/FoundationRimac.h
@@ -0,0 +1,18 @@
+//
+//  FoundationRimac.h
+//  FoundationRimac
+//
+//  Created by Irwin Bravo Oporto on 14/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FoundationRimac.
+FOUNDATION_EXPORT double FoundationRimacVersionNumber;
+
+//! Project version string for FoundationRimac.
+FOUNDATION_EXPORT const unsigned char FoundationRimacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoundationRimac/PublicHeader.h>
+
+
